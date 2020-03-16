1a865b954c79627eb170116b9a98230ac1e07cab
terataichi <taichiz519@gamil.com>
2020-03-16 14:56:32 +0900

    1

---
+++ unity t/TestFlappyBird/Assets/Block.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    Rigidbody2D _rigidbody;
+    public float _speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 物理挙動コンポーネートの格納
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _speed = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 _position = transform.position;
+
+        _rigidbody.velocity = Vector2.zero;                     // 落下速度を一度リセットする
+
+        _rigidbody.AddForce(new Vector2(-_speed, 0));
+
+        // 壁ﾁｪｯｸ
+        if(_position.x < GetLeft())
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // 画面の左下の座標を取得する
+    float GetLeft()
+    {
+        Vector2 min = Camera.main.ViewportToWorldPoint(Vector2.zero);
+
+        return min.x;
+    }
+
+    public void SetSpeed(float speed)
+    {
+        _speed = speed;
+    }
+
+}
---
+++ unity t/TestFlappyBird/Assets/BlockMng.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockMng : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // 生成するﾌﾞﾛｯｸ
+    public GameObject block;
+
+    // 0になったらﾌﾞﾛｯｸを生成する
+    float _timer;
+
+    // トータル時間保持用
+    float _totalTime;
+
+    void Start()
+    {
+        _timer = 0;
+        _totalTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _timer -= Time.deltaTime;
+
+        _totalTime += Time.deltaTime;
+
+        if(_timer <= 0)
+        {
+            // 0になったのでBlock生成
+            // BlockMngの場所から生成
+            Vector3 position = transform.position;
+
+            // プレハブを元にBlock生成
+            GameObject obj = Instantiate(block, position, Quaternion.identity);
+
+            // ﾌﾞﾛｯｸの情報取得
+            Block blockScript = obj.GetComponent<Block>();
+
+            // 速度を計算して設定
+            // 基本速度100に、経過時間*10を加える
+            float speed = blockScript._speed + (_totalTime * 10);
+
+            blockScript.SetSpeed(-speed);
+
+            // リセット
+            _timer += 1;
+        }
+    }
+}
